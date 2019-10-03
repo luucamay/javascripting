05cd87d5a1a06a2c63fcf81945282c474573c97d
luucamay <lupemaydana@gmail.com>
2019-10-02 22:36:12 -0400

    added functions to printList and remove_duplicates

--- must-have-knowledge/linkedList.js
+++ must-have-knowledge/linkedList.js
@@ -3,15 +3,53 @@ class Node{
 		this.data = data
 		this.next = null
 	}
+			
 }
 
-// create first node
-const head = new Node(8)
+function printList(node){
+	let string = ''
+	while(node!=null){
+		string += node.data + ' '
+		node = node.next
+	}
+	return string
+}
 
-// add a second node
-head.next = new Node(12)
+function remove_duplicates(node){
+	let uniques = []
+	let previous = new Node(null)
+	while(node!=null){
+		if(uniques.includes(node.data)){
+			previous.next = node.next
+		} else {
+			uniques.push(node.data)
+			previous = node
+		}
+		node = node.next
+	}
+}
 
-// add a third node
-head.next.next = new Node(8)
 
-console.log(head)
+let node1 = new Node(8)
+let node2 = new Node(12)
+let node3 = new Node(13)
+let node4 = new Node(8)
+let node5 = new Node(7)
+let node6 = new Node(13)
+let node7 = new Node(13)
+let node8 = new Node(13)
+let node9 = new Node(11)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = node8
+node8.next = node9
+
+console.log(printList(node1))
+
+remove_duplicates(node1)
+console.log(printList(node1))
+
