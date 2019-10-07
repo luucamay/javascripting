54ed10e59ebf165b54e59d7ba9f4201fad14fd62
luucamay <lupemaydana@gmail.com>
2019-10-07 18:12:53 -0400

    insert_edge function defined for graph class

--- must-have-knowledge/graph.js
+++ must-have-knowledge/graph.js
@@ -14,7 +14,7 @@ class Edge{
 }
 
 class Graph{
-	constructor(nodes, edges){
+	constructor(nodes = [], edges = []){
 		this.nodes = nodes
 		this.edges = edges
 	}
@@ -25,6 +25,34 @@ class Graph{
 	insert_edge(new_edge_val, node_from_val, node_to_val){
 		let from_found = null
 		let to_found = null
-		// keep building
+		let nodes_length = this.nodes.length
+		for (let i = 0; i < nodes_length; i++){
+			let node = this.nodes[i]
+			if (node_from_val == node.value){
+				from_found = node
+			}
+			if (node_to_val == node.value){
+				to_found = node
+			}
+		}
+		if (from_found == null){
+			from_found = new Node(node_from_val)
+			this.nodes.push(from_found)
+		}
+		if (to_found == null){
+			to_found = new Node(node_to_val)
+			this.nodes.push(to_found)
+		}
+		let new_edge = new Edge(new_edge_val, from_found, to_found)
+		from_found.edges.push(new_edge)
+		to_found.edges.push(new_edge)
+		this.edges.push(new_edge)
 	}
 }
+
+let graph = new Graph()
+graph.insert_edge(100,1,2)
+graph.insert_edge(101,1,3)
+graph.insert_edge(102,1,4)
+graph.insert_edge(103,3,4)
+console.log(graph)
