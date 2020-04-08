45db46dcc7494a029c42b3081bc3ff15bb55dff8
luucamay <lupemaydana@gmail.com>
2020-04-08 00:36:36 -0400

    Solving day 2 challenge!

---
+++ 30DaysOfJavaScript/day2/index.js
@@ -0,0 +1,10 @@
+let challenge = '30 Days Of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+let challengeUpper = challenge.toUpperCase()
+let challengeLower = challenge.toLowerCase()
+//challenge = challenge[0,4] // does it works?
+console.log(challenge.includes('Script'))
+let challengeArray = challenge.split('')
+challengeArray = challenge.split(' ')
+// Falta reto 6,7 y 10 en adelante
