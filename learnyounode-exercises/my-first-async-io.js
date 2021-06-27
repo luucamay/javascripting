d8b0916df767c0d28ce4c21a51f7754fa008ad28
Lupe <lupemaydana@gmail.com>
2021-06-27 09:51:32 -0700

    feat: Read a fyle in a asyn way

---
+++ learnyounode-exercises/my-first-async-io.js
@@ -0,0 +1,11 @@
+const { throws } = require('assert');
+const fs = require('fs');
+
+const countLines = (err, data) => {
+  if (err)
+    return console.log(err);
+  const lines = data.split('\n');
+  console.log(lines.length - 1);
+}
+
+fs.readFile(process.argv[2], 'utf-8', countLines);
