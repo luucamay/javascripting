3dadb8d40c70f27f379d46541f74edc82de931e3
Lupe <lupemaydana@gmail.com>
2021-12-22 12:00:56 -0400

    clean folder

--- 10.for-loop.js
+++
@@ -1,6 +0,0 @@
-let total = 0
-const limit = 10
-for (let i=0; i < limit; i++){
-	total += i
-}
-console.log(total)
--- 11.arrays.js
+++
@@ -1,3 +0,0 @@
-let pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni']
-console.log(pizzaToppings)
-
