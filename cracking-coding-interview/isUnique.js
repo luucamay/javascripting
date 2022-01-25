39c3a486491cd40eebb850baf7ed645a19a93ae5
Lupe <lupemaydana@gmail.com>
2022-01-24 23:42:44 -0800

    feat: Add the solution with sorting

--- cracking-coding-interview/isUnique.js
+++ cracking-coding-interview/isUnique.js
@@ -1,16 +1,28 @@
 // check if a string has all unique characters.
-// What if you can not use additional data structures
+// What if you can not use additional data structures *in this case sort first then compare current and after
+// Hints: #44, #177, #132
 const isUnique = (xString) => {
   const dictionary = {}
   for (const char of xString) {
-    if (!dictionary.hasOwnProperty(char)) {
-      dictionary[char] = 1;
-    } else {
+    if (dictionary.hasOwnProperty(char)) {
       return false;
     }
+    dictionary[char] = 1;
   }
   return true;
 }
 
+const isUniqueSort = (xString) => {
+  const xArray = xString.split('').sort();
+  const size = xString.length;
+  let unique = true;
+  xArray.forEach((char, index) => {
+    if (index < size - 1 && char === xArray[index + 1]) {
+      unique = false;
+    }
+  });
+  return unique;
+}
 const result = isUnique("asdfg");
-console.log(result);
+const result2 = isUniqueSort("basdfg");
+console.log(result, result2);
